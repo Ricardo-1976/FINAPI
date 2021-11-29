b3bdf2fa4531888080111358e8fb8a2ef844eec7
Ricardo-1976 <ricardojoaoantonio2@gmail.com>
2021-11-29 15:33:06 +0100

    Update index.js
    b

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ app.use(express.json());
 
 customers = [];
 
+function VeriflyIsExistsAccountCPF (request, response, next){
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if(!customer) {
+    return response.status(400).json({ error: "Customer not found"})
+  }
+  request.customer = customer;
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const {name , cpf} = request.body;
 
@@ -26,13 +38,26 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) => {
-  
-  const { cpf } = request.params;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
+app.get("/statement", VeriflyIsExistsAccountCPF, (request, response) => {
+  const {customer} = request;
   
   return response.json(customer.statement);
+});
+
+app.post("/deposit", VeriflyIsExistsAccountCPF, (request, response) =>{
+  const { description, amount} = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    creatd_at: new Date(),
+    type: "credit",
+  };
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
